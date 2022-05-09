3e651a37d7cfb957e7850851871874190a7f2fac
yaswanth14 <yaswanth14333@gmail.com>
2022-05-09 20:15:59 +0530

    added fake data gfor seeding in database

--- app.js
+++ app.js
@@ -26,10 +26,9 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makeCampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap campaign'});
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
