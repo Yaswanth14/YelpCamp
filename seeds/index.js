05cf73946e6e2f8d31a65ccc518a6979ffb3e7d0
yaswanth14 <yaswanth14333@gmail.com>
2022-05-12 16:54:41 +0530

    added footer and updated database with images and description

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const cities = require('.cities');
+const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
@@ -23,9 +23,13 @@ const seedDB = async () => {
     // await c.save();
     for(let i=0; i<50; i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi eaque nihil, est voluptas amet consequuntur earum autem? Dolorem blanditiis laudantium, dolore rem doloremque earum quod delectus asperiores dolorum qui. Reprehenderit?',
+            price
         })
         await camp.save();
     }
